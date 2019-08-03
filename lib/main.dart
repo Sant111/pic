7768b9e4b0fffb5cf9087850fde869559454cd5b
An-Tim <an-tim.nguyen@unbabel.com>
2019-08-03 13:05:56 +0100

    Add project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+// Define a 'main' function to run when our app starts
+void main(){
+
+// Take that widget and get it on the screen.
+runApp(App());
+}
